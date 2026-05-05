0cb63d1e61b503732e6a33d2ce0b150b8ffb4adb
shivansh11-ui <shivanshmishrashiva1@gmail.com>
2026-05-05 19:22:04 +0530

    Connect database before server starts

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const app = require("./app");
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Backend running on port ${port}`);
+app.connectDatabase().then(() => {
+  app.listen(port, () => {
+    console.log(`Backend running on port ${port}`);
+  });
 });
